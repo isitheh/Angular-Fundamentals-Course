7fe3368ad32685724426f1a1175000205c5d52b7
isitheh@gmail.com <isitheh@gmail.com>
2024-03-13 21:43:25 +0200

    Angular Bootstrap

--- Angular Essential Training/hello-world/src/app/app.component.ts
+++ Angular Essential Training/hello-world/src/app/app.component.ts
@@ -5,6 +5,10 @@ import { ImagecomponentComponent } from './imagecomponent/imagecomponent.compone
 import { EmpInfoComponent } from './emp-info/emp-info.component';
 import { FormComponentComponent } from './form-component/form-component.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from  '../app/Components/navbar/navbar.component';
+import { IntroComponent } from '../app/Components/intro/intro.component';
+import { CoursesComponent } from '../app/Components/courses/courses.component';
+import { FooterComponent } from '../app/Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +19,11 @@ import { FormsModule } from '@angular/forms';
     ImagecomponentComponent, 
     EmpInfoComponent,
     FormsModule,
-    FormComponentComponent
+    FormComponentComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
